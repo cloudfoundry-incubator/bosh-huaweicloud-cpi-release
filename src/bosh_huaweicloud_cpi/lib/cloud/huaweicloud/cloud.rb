e196938f8d78aea9ef0d3d891d1acd77a0e6ef5b
zhongjun2 <jun.zhongjun@huawei.com>
2018-08-02 19:11:46 +0800

    Change config name

--- src/bosh_huaweicloud_cpi/lib/cloud/huaweicloud/cloud.rb
+++ src/bosh_huaweicloud_cpi/lib/cloud/huaweicloud/cloud.rb
@@ -31,7 +31,7 @@ module Bosh::HuaweiCloud
       @logger = Bosh::Clouds::Config.logger
 
       @agent_properties = @options.fetch('agent', {})
-      openstack_properties = @options['openstack']
+      openstack_properties = @options['huaweicloud']
       @default_key_name = openstack_properties['default_key_name']
       @default_security_groups = openstack_properties['default_security_groups']
       @default_volume_type = openstack_properties['default_volume_type']
@@ -43,7 +43,7 @@ module Bosh::HuaweiCloud
       @use_config_drive = !!openstack_properties.fetch('config_drive', false)
       @config_drive = openstack_properties['config_drive']
 
-      @openstack = Bosh::HuaweiCloud::Huawei.new(@options['openstack'])
+      @openstack = Bosh::HuaweiCloud::Huawei.new(@options['huaweicloud'])
 
       @az_provider = Bosh::HuaweiCloud::AvailabilityZoneProvider.new(
         @openstack,
@@ -499,7 +499,7 @@ module Bosh::HuaweiCloud
     end
 
     def is_v3
-      @options['openstack']['auth_url'].match(/\/v3(?=\/|$)/)
+      @options['huaweicloud']['auth_url'].match(/\/v3(?=\/|$)/)
     end
 
     ##
@@ -699,11 +699,11 @@ module Bosh::HuaweiCloud
     # @return [void]
     # @raise [ArgumentError] if options are not valid
     def validate_options
-      raise ArgumentError, "Invalid OpenStack cloud properties: No 'openstack' properties specified." unless @options['openstack']
-      auth_url = @options['openstack']['auth_url']
+      raise ArgumentError, "Invalid OpenStack cloud properties: No 'openstack' properties specified." unless @options['huaweicloud']
+      auth_url = @options['huaweicloud']['auth_url']
       schema = Membrane::SchemaParser.parse do
         openstack_options_schema = {
-          'openstack' => {
+          'huaweicloud' => {
             'auth_url' => String,
             'username' => String,
             'api_key' => String,
